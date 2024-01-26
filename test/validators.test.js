f182898c02ed6188fdcfd6c707c0875819e5d1c9
Anurag Vutukuri <>
2024-01-26 15:30:57 -0600

    Added unit tests for validator.js

--- test/validators.test.js
+++ test/validators.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { validateButterfly, validateUser } = require('../src/validators');
+const { validateButterfly, validateButterflyRating, validateUser } = require('../src/validators');
 
 describe('validateButterfly', () => {
   const validButterfly = {
@@ -30,6 +30,86 @@ describe('validateButterfly', () => {
       validateButterfly({
         extra: 'field',
         ...validButterfly
+      });
+    }).toThrow('The following keys are invalid: extra');
+  });
+});
+
+describe('validateButterflyRating', () => {
+  const validButterflyRating = {
+    id: 'wxyz98762',
+    userId: 'abcd1234',
+    rating: 4
+  };
+
+  const validButterflyRatingWithReview = {
+    id: 'wxyz98762',
+    userId: 'abcd1234',
+    rating: 4,
+    review: 'Good butterfly'
+  };
+
+  it('is ok for a valid rating', () => {
+    const result = validateButterflyRating(validButterflyRating);
+    expect(result).toBe(undefined);
+  });
+
+  it('is ok for a valid rating with a review ', () => {
+    const result = validateButterflyRating(validButterflyRatingWithReview);
+    expect(result).toBe(undefined);
+  });
+
+  it('throws an error when invalid', () => {
+    expect(() => {
+      validateButterflyRating({});
+    }).toThrow('The following properties have invalid values:');
+
+    expect(() => {
+      validateButterflyRating({
+        ...validButterflyRating,
+        id: 123
+      });
+    }).toThrow('id must be a string.');
+
+    expect(() => {
+      validateButterflyRating({
+        ...validButterflyRating,
+        userId: 123
+      });
+    }).toThrow('userId must be a string.');
+
+    expect(() => {
+      validateButterflyRating({
+        ...validButterflyRating,
+        rating: '4'
+      });
+    }).toThrow('rating must be a number between 0 & 5 (inclusive).');
+
+    expect(() => {
+      validateButterflyRating({
+        ...validButterflyRating,
+        rating: -1
+      });
+    }).toThrow('rating must be a number between 0 & 5 (inclusive).');
+
+    expect(() => {
+      validateButterflyRating({
+        ...validButterflyRating,
+        rating: 6
+      });
+    }).toThrow('rating must be a number between 0 & 5 (inclusive).');
+
+    expect(() => {
+      validateButterflyRating({
+        ...validButterflyRatingWithReview,
+        review: 100
+      });
+    }).toThrow('review must be a string.');
+
+    expect(() => {
+      validateButterflyRating({
+        extra: 'field',
+        ...validButterflyRating
       });
     }).toThrow('The following keys are invalid: extra');
   });
